3db98f80fca8a699ba3d56b7470cde973d8b3784
Jacob Kucera <kucerajacob@gmail.com>
2020-11-04 05:54:53 -0800

    fixed bugs + added nice theme
    still WIP

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const pads = document.querySelector(".parent").children;
+const rows = document.querySelector(".sequencer").children;
 
 const kick = new Audio("/audio/kick.wav"),
-clap = new Audio("/audio/clap.wav"),
-hihat = new Audio("/audio/hihat.wav"),
-rim = new Audio("/audio/rim.wav");
+	clap = new Audio("/audio/clap.wav"),
+	hihat = new Audio("/audio/hihat.wav"),
+	rim = new Audio("/audio/rim.wav");
 
-const item = document.querySelectorAll(".item");
+const item = document.querySelectorAll(".sample");
 
 // Checkbox toggle functionality
 item.forEach(function (el) {
@@ -18,34 +18,50 @@ item.forEach(function (el) {
 	}
 });
 
-
-// Looping mechanism
 var i = -1;
 
-setInterval(function() {
-	i++;
+function rowLoop() {
+	setInterval(function () {
+		i++;
 
-	if (i === pads.length) {
-		i = 0;
-		document.querySelector(".item:nth-child(" + pads.length + ")").classList.remove("row");
-	}
-
-	let booba = document.querySelector(".parent").children.item(i);
-	booba.classList.add("row");
-
-	if (i > 0) {
-		document.querySelector(".item:nth-child(" + i + ")").classList.remove("row");
-	}
-
-	console.log(i);
-
-	item.forEach(function (bruh) {
-		if (bruh.classList.contains("row") && bruh.classList.contains("item-selected")) {
-			kick.play();
+		if (i === rows.length) {
+			i = 0;
+			document.querySelector(".d16").childNodes[1].classList.remove("row-highlight");
+			document.querySelector(".d16").childNodes[3].classList.remove("row-highlight");
+			document.querySelector(".d16").childNodes[5].classList.remove("row-highlight");
+			document.querySelector(".d16").childNodes[7].classList.remove("row-highlight");
 		}
-	});
-}, 100);
 
-function playSound() {
-	kick.play();
+		document.querySelector(".d" + (i + 1)).childNodes[1].classList.add("row-highlight");
+		document.querySelector(".d" + (i + 1)).childNodes[3].classList.add("row-highlight");
+		document.querySelector(".d" + (i + 1)).childNodes[5].classList.add("row-highlight");
+		document.querySelector(".d" + (i + 1)).childNodes[7].classList.add("row-highlight");
+
+		if (i > 0) {
+			document.querySelector(".d" + i).childNodes[1].classList.remove("row-highlight");
+			document.querySelector(".d" + i).childNodes[3].classList.remove("row-highlight");
+			document.querySelector(".d" + i).childNodes[5].classList.remove("row-highlight");
+			document.querySelector(".d" + i).childNodes[7].classList.remove("row-highlight");
+		}
+
+		document.querySelectorAll(".d" + (i + 1)).forEach(function (bruh) {
+			if (bruh.childNodes[1].classList.contains("row-highlight") && bruh.childNodes[1].classList.contains("item-selected")) {
+				kick.play();
+			}
+
+			if (bruh.childNodes[3].classList.contains("row-highlight") && bruh.childNodes[3].classList.contains("item-selected")) {
+				clap.play();
+			}
+
+			if (bruh.childNodes[5].classList.contains("row-highlight") && bruh.childNodes[5].classList.contains("item-selected")) {
+				hihat.play();
+			}
+
+			if (bruh.childNodes[7].classList.contains("row-highlight") && bruh.childNodes[7].classList.contains("item-selected")) {
+				rim.play();
+			}
+		});
+	}, 150);
 }
+
+rowLoop();
